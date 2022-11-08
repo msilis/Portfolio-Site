16ff7a3f5b5672d9be5db9db9356d95d8cd10364
Miks Silis <109594498+msilis@users.noreply.github.com>
2022-11-08 22:06:11 +0000

    Add files via upload
    Updates to projects tab

--- script.js
+++ script.js
@@ -1,47 +1,3 @@
-var i = 0;
-var images = [];
-var time = 3000;
-var text = [];
-var link = [];
-
-//Image list
-
-images[0] = 'media/miksDigsHolesScreenshot.png';
-images[1] = 'media/pepperShakersScreenshot.png';
-images[2] = 'media/teaCozyScreenshot.png';
-
-//Caption list
-
-text[0] = 'Responsive website for building company.';
-text[1] = 'HTML/CSS website landing page for a club.';
-text[2] = 'Responsive langing page for a tea company.';
-
-//Link list
-
-link[0] = 'https://msilis.github.io/digsholes/';
-link[1] = 'https://msilis.github.io/peppershakers/';
-link[2] = 'https://msilis.github.io/teacozy';
-
-//Slideshow function
-
-function changeImage() {
-    document.slide.src = images[i];
-    document.getElementById("slide-text").innerHTML = text[i];
-    document.getElementById('image-link').setAttribute('href', link[i]);
-
-    if(i < images.length -1){
-        i++;
-        
-    } else {
-        i = 0;
-    }
-    setTimeout('changeImage()', time);
-    
-}
-
-
-window.onload = changeImage;
-
 //Navigation Buttons
 
 const project = document.getElementById('project-section');
@@ -53,7 +9,7 @@ const topOfPage = document.getElementById('top');
 /*Navigate to sections*/
 
 document.getElementById('project-button').addEventListener('click', ()=>{
-    project.style.display = 'block';
+    project.style.display = 'flex';
     about.style.display = 'none';
     contact.style.display = 'none';
     footer.style.display = 'none';
